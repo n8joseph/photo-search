37d4cda0a42efac448c58632c52d1fcbbba4bfc7
n8joseph <natejoseph@Nates-MBP.home>
2015-09-28 20:34:57 -0400

    added basic search functionality

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
 
 
-angular.module('app', [])
-.config(function($httpProvider) {
-	$httpProvider.defaults.useXDomain = true;
-})
-.controller('MyController', function($scope, $http, $sce) {
-	$scope.embedUrl = "ENTER HERE";
+angular.module('MyApp', [])
+	.config(function($httpProvider) {
+		$httpProvider.defaults.useXDomain = true;
+	})
+	.controller('MyController', function($scope, $http, $sce) {
+	
+		$scope.embedUrl = "ENTER HERE";
 
-	$scope.trustSrc = function(src) {
-		return $sce.trustAsResource(src);
+		$scope.trustSrc = function(src) {
+			return $sce.trustAsResource(src);
+		};
+
+		$scope.searchInstagram = function(keyword) {
+			
+			$scope.keyword = $scope.searchForm.keyword.$viewvalue;
+
+			var url = "https://api.instagram.com/v1/tags/" + keyword + "/media/recent";
+
+			var request = {
+				callback: 'JSON_CALLBACK',
+				client_id: '541fd0be5de2463aa359f64e029722cd',
+				count: 8
+			};
+
+			$http({
+				method: 'JSONP',
+				url: url,
+				params: request
+			})
+			.then(function(response) {
+				console.log('success!')
+				$scope.results = response.data.data[0].images.standard_resolution.url
+				console.log(response)
+			},
+			function(response) {
+				alert('error')
+			})
 	};
 
-	$scope.searchInstagram = function (keyword) {
 
-
-
-	};
 });
